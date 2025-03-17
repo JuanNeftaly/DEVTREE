6cb37ccd8caa345b57c8bb98ba25b1e339ef3def
JuanNeftaly <00182222@uca.edu.sv>
2025-03-17 10:39:17 -0600

    Add: Req & Res

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,8 @@
 //funciones q se mandan a llamar
-
+import type {Request, Response} from 'express'
 import User from "../models/User";
 
- export const createAccount = async (req, res) =>{
+ export const createAccount = async (req : Request, res : Response) =>{
 	console.log('User register');
 	
 	const user = new User(req.body)
