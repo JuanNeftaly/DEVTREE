1982871e1f100cd06a6dc88c270f38b10c36567f
JuanNeftaly <00182222@uca.edu.sv>
2025-03-17 08:54:31 -0600

    Add: Types & Interfaces

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,22 @@ server.listen(port, () => {
 	console.log(colors.blue.bold('Servidor funcionando en el puerto:'), port);
 })
 
+// creacion de types 
+type Product = {
+	id: number
+	price: number
+	name: string
+}
+
+// asignar types 
+let product : Product = {
+	id: 1,
+	price: 45,
+	name: 'Cebollas'
+}
+
+// creacion Interfaces
+interface Client {
+	id: number
+	name: string
+}
